d4d9326b73a495b07a512e813cd788feea702f2e
Shunsuke Hiratsuka <hiratsukashu@indival.co.jp>
2018-01-21 12:36:59 +0900

    初期コミット。簡単な会話の仕方覚えた

---
+++ app/Http/Controllers/BotManController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use BotMan\BotMan\BotMan;
+use Illuminate\Http\Request;
+use App\Conversations\ExampleConversation;
+
+class BotManController extends Controller
+{
+    /**
+     * Place your BotMan logic here.
+     */
+    public function handle()
+    {
+        $botman = app('botman');
+
+        $botman->hears('Hello', function ($bot) {
+            $bot->reply('Hello! Shunsuke');
+        });
+        $botman->hears('How are you?', function ($bot) {
+            $bot->reply("I'm fine.");
+        });
+
+        $botman->listen();
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function tinker()
+    {
+        return view('tinker');
+    }
+
+    /**
+     * Loaded through routes/botman.php
+     * @param  BotMan $bot
+     */
+    public function startConversation(BotMan $bot)
+    {
+        $bot->startConversation(new ExampleConversation());
+    }
+}
